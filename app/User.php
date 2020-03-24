966e2cdd01e57f1c7529931e2b32a8bc67bfb25d
Valdimir <elcopro@gmail.com>
2020-03-24 21:59:32 +0300

    Permission & Passport

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
