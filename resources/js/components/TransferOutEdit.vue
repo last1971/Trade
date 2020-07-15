d28f818d0ae6f13f2c8fb050585828b7ee434dac
Vladimir <elcopro@gmail.com>
2020-07-15 09:12:47 +0300

    TransferOut Amount in Edit

--- resources/js/components/TransferOutEdit.vue
+++ resources/js/components/TransferOutEdit.vue
@@ -39,6 +39,9 @@
             <v-col cols="12" sm="auto" v-if="!notCan">
                 <v-text-field label="Примечание" v-model="model.PRIM"/>
             </v-col>
+            <v-col cols="12" sm="auto">
+                <v-text-field label="Сумма" :value="model.transferOutLinesSum | formatRub" disabled/>
+            </v-col>
             <v-col class="d-flex justify-center align-center" cols="12" sm="auto">
                 <router-link :to="{ name: 'invoice', params: { id: model.SCODE } }"
                              class="title"
