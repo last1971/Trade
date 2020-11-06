b20bcd2073621c6c7251dc9bf8ecc70fdb209e51
Vladimir <elcopro@gmail.com>
2020-11-06 11:39:40 +0300

    remove XML_PRIM

--- app/Buyer.php
+++ app/Buyer.php
@@ -13,6 +13,8 @@ class Buyer extends Model
 
     protected $connection = 'firebird';
 
+    protected $hidden = ['XML_PRIM'];
+
     protected $primaryKey = 'POKUPATCODE';
 
     protected $table = 'POKUPAT';
