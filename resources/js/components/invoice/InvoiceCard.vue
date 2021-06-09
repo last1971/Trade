c9e912a7d2bba6f77e1b8dfdd767de92ccabb616
Vladimir <elcopro@gmail.com>
2021-06-09 21:02:32 +0300

    InvoiceCard.vue continue

--- resources/js/components/invoice/InvoiceCard.vue
+++ resources/js/components/invoice/InvoiceCard.vue
@@ -1,7 +1,33 @@
 <template>
     <v-card class="m-1">
         <v-card-title class="text-center">
-            <invoice-select v-model="currentInvoice" />
+            <v-edit-dialog  ref="dialog">
+                <v-container v-if="currentInvoice">
+                    <v-row>
+                        <v-col>
+                            № {{ invoice.NS }} от {{ invoice.DATA | formatDate }}
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            {{ invoice.buyer.SHORTNAME }}
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        {{ invoice.invoiceLinesCount }} строк на {{ invoice.invoiceLinesSum | formatRub }}
+                    </v-row>
+                </v-container>
+                <v-container v-else>
+                    <v-row>
+                        <v-col>
+                            В Ы Б Е Р И С Ч Е Т
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <template v-slot:input>
+                    <invoice-select v-model="currentInvoice" />
+                </template>
+            </v-edit-dialog>
         </v-card-title>
     </v-card>
 </template>
@@ -19,6 +45,9 @@ export default {
             set(currentInvoice) {
                 this.$store.commit('INVOICE/SET-CURRENT', currentInvoice);
             }
+        },
+        invoice() {
+            return this.$store.getters['INVOICE/GET'](this.currentInvoice);
         }
     }
 }
