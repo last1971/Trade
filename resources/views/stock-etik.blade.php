7632f7f70001d93504ac798ebadbf66d14b0aaa4
Vladimir Lastovka <elcopro@gmail.com>
2023-05-30 15:05:36 +0300

    font size

--- resources/views/stock-etik.blade.php
+++ resources/views/stock-etik.blade.php
@@ -11,38 +11,30 @@
     <body>
         @foreach($pickUps as $pickUp)
         <div style="page-break-after: {{ $loop->last ? 'avoid' : 'always' }}">
-            <p style="font-size: 10px; text-align: center;">{{ $invoice->firm->FIRMNAME }}</p>
-        <table style="font-size: 8px; border-spacing: 0px;">
+            <p style="font-size: 11px; text-align: center;">{{ $invoice->firm->FIRMNAME }}</p>
+        <table style="border-spacing: 0px;">
             <tbody>
-                <tr>
+                <tr style="font-size: 9px;" >
                     <td><b>Наименование:</b></td>
                     <td>{{ $pickUp->good->name->NAME }}</td>
                 </tr>
-                <tr>
-                    <td><b>Корпус:</b></td>
-                    <td>{{ $pickUp->good->BODY }}</td>
-                </tr>
-                <tr>
-                    <td><b>Производитель:</b></td>
-                    <td>{{ $pickUp->good->PRODUCER }}</td>
-                </tr>
-                <tr>
+                <tr style="font-size: 9px;">
                     <td><b>Покупатель:</b></td>
-                    <td>{{ $invoice->buyer->FULLNAME }}</td>
+                    <td>{{ $invoice->buyer->SHORTNAME }}</td>
                 </tr>
-                <tr>
+                <tr style="font-size: 13px;">
                     <td><b>Номер счета:</b></td>
                     <td>{{ $invoice->NS }}</td>
                 </tr>
-                <tr>
+                <tr style="font-size: 13px;">
                     <td><b>Количество:</b></td>
                     <td>{{ $pickUp->QUANSKLADNEED - $pickUp->QUANSKLAD }}</td>
                 </tr>
-                <tr>
+                <tr style="font-size: 7px;">
                     <td><b>Подобрано:</b></td>
                     <td>{{ \Carbon\Carbon::now()->format('d.m.Y H:i') }}</td>
                 </tr>
-                <tr>
+                <tr style="font-size: 7px;">
                     <td><b>Сотрудник</b></td>
                     <td>{{ $employee->FULLNAME }}</td>
                 </tr>
