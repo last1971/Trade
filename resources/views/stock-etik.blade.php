83f0ae507cfceb5955e3bf1d355a439445508615
Vladimir Lastovka <elcopro@gmail.com>
2023-08-15 15:10:04 +0300

    padding

--- resources/views/stock-etik.blade.php
+++ resources/views/stock-etik.blade.php
@@ -14,28 +14,30 @@
             <tbody>
                 <tr>
                     <td style="font-size: 10px;">Наименование:</td>
-                    <td style="font-size: 10px;"><b>{{ $pickUp->good->name->NAME }}</b></td>
+                    <td style="font-size: 10px; padding: 2px;">{{ $pickUp->good->name->NAME }}</td>
                 </tr>
                 <tr>
                     <td style="font-size: 10px;">Покупатель:</td>
-                    <td style="font-size: 12px;"><b>{{ $pickUp->invoiceLine->invoice->buyer->SHORTNAME }}</b></td>
+                    <td style="font-size: 10px; padding: 2px;">{{ $pickUp->invoiceLine->invoice->buyer->SHORTNAME
+                    }}</td>
                 </tr>
                 <tr>
                     <td style="font-size: 10px;">Номер счета:</td>
-                    <td style="font-size: 16px;"><b>{{ $pickUp->invoiceLine->invoice->NS }}</b></td>
+                    <td style="font-size: 14px; padding: 2px;"><b>{{ $pickUp->invoiceLine->invoice->NS }}</b></td>
                 </tr>
                 <tr>
                     <td style="font-size: 10px;">Количество:</td>
-                    <td style="font-size: 20px;"><b>{{ $pickUp->QUANSKLADNEED -
+                    <td style="font-size: 20px; padding: 2px;"><b>{{ $pickUp->QUANSKLADNEED -
                     $pickUp->QUANSKLAD }}</b></td>
                 </tr>
                 <tr>
                     <td style="font-size: 10px;">Подобрано:</td>
-                    <td style="font-size: 10px;"><b>{{ \Carbon\Carbon::now()->format('d.m.Y H:i') }}</b></td>
+                    <td style="font-size: 10px; padding: 2px;"><b>{{ \Carbon\Carbon::now()->format('d.m.Y H:i')
+                    }}</b></td>
                 </tr>
                 <tr>
                     <td style="font-size: 10px;">Сотрудник</td>
-                    <td style="font-size: 10px;"><b>{{ $employee->FULLNAME }}</b></td>
+                    <td style="font-size: 10px; padding: 2px;">{{ $employee->FULLNAME }}</td>
                 </tr>
             </tbody>
         </table>
