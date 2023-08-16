2e69c699e0b5eea95a03c23f008ca5d59a2aea18
Vladimir Lastovka <elcopro@gmail.com>
2023-08-16 12:18:25 +0300

    size3

--- resources/views/stock-etik.blade.php
+++ resources/views/stock-etik.blade.php
@@ -17,26 +17,26 @@
                     }}</b></td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px;">Покупатель:</td>
+                    <td style="font-size: 8px;">Покупатель:</td>
                     <td style="font-size: 10px;">{{ $pickUp->invoiceLine->invoice->buyer->SHORTNAME
                     }}</td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px;">Номер счета:</td>
-                    <td style="font-size: 14px;"><b>{{ $pickUp->invoiceLine->invoice->NS }}</b></td>
+                    <td style="font-size: 8px;">Номер счета:</td>
+                    <td style="font-size: 10px;">{{ $pickUp->invoiceLine->invoice->NS }}</td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px;">Количество:</td>
-                    <td style="font-size: 18px;"><b>{{ $pickUp->QUANSKLADNEED -
+                    <td style="font-size: 8px;">Количество:</td>
+                    <td style="font-size: 14px;"><b>{{ $pickUp->QUANSKLADNEED -
                     $pickUp->QUANSKLAD }}</b></td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px;">Подобрано:</td>
-                    <td style="font-size: 10px;"><b>{{ \Carbon\Carbon::now()->format('d.m.Y H:i')
-                    }}</b></td>
+                    <td style="font-size: 8px;">Подобрано:</td>
+                    <td style="font-size: 8px;">{{ \Carbon\Carbon::now()->format('d.m.Y H:i')
+                    }}</td>
                 </tr>
                 <tr>
-                    <td style="font-size: 10px;">Сотрудник</td>
+                    <td style="font-size: 8px;">Сотрудник</td>
                     <td style="font-size: 10px;">{{ $employee->FULLNAME }}</td>
                 </tr>
             </tbody>
